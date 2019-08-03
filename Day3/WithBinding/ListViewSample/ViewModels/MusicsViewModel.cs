3b953ee4dcb2b4cbb0666f6c8a221ad0e9870e84
Enmanuel Toribio <enmanuel@megsoftconsulting.com>
2019-08-03 12:06:31 -0400

    Adding binded example

---
+++ Day3/WithBinding/ListViewSample/ViewModels/MusicsViewModel.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using ListViewSample.Models;
+using ListViewSample.Views;
+using Xamarin.Forms;
+
+namespace ListViewSample.ViewModels
+{
+    public class MusicsViewModel : INotifyPropertyChanged
+    {
+        public ObservableCollection<Musics> Musics { get; set; } 
+
+        public MusicsViewModel()
+        {
+            Musics = new ObservableCollection<Musics> 
+            {
+                new Musics 
+                {
+                    Picture     = "Music",
+                    Name        = "Las Abejas",
+                    Description ="Cancion de Juan Luis Guerra"
+                },
+                new Musics
+                {
+                    Picture     = "Music",
+                    Name        = "Bachata Rosa",
+                    Description = "Cancion de Juan Luis Guerra"
+                },
+                new Musics
+                {
+                    Picture     = "Music",
+                    Name        = "Un hombre normal",
+                    Description = "Cancion de Ricardo Montaner 2012"
+                },
+                new Musics
+                {
+                    Picture     = "Music",
+                    Name        = "La bilirubina Rosa",
+                    Description = "Cancion de Juan Luis Guerra"
+                },
+                new Musics
+                {
+                    Picture     = "Music",
+                    Name        = "Minutos",
+                    Description = "Cancion de Ricardo Arjona"
+                },
+                new Musics
+                {
+                    Picture     = "Music",
+                    Name        = "La cima del cielo",
+                    Description = "Cancion de Ricardo Montaner"
+                }
+            }; 
+        } 
+        
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
---
+++ Day3/WithBinding/ListViewSample/Views/MusicsPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using ListViewSample.Models;
+using Xamarin.Forms;
+
+namespace ListViewSample.Views
+{
+    public partial class MusicsPage : ContentPage
+    {
+         public MusicsPage()
+        {
+            InitializeComponent();
+        }
+
+        async void Item_Clicked(object sender, System.EventArgs e)
+        {
+            var musicDetailPage = new MusicDetailPage();
+            musicDetailPage.BindingContext = MusicLitsView.SelectedItem as Musics;
+            await Navigation.PushAsync(musicDetailPage);  
+        }
+    }
+}
