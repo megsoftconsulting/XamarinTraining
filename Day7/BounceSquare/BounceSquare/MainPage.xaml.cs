6e6be7edad5530b810c337eccd1a2d4ef1feb3fe
Enmanuel Toribio <enmanuel@megsoftconsulting.com>
2019-08-13 12:48:32 -0400

    Adding extra samples

---
+++ Day7/BounceSquare/BounceSquare/MainPage.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace BounceSquare
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public async void Handle_Tapped(object sender, EventArgs e)
+        {
+            await myBouncingBox.FadeTo(0, 500, Easing.BounceIn);
+            await myBouncingBox.FadeTo(1, 500, Easing.BounceIn);
+            await myBouncingBox.ScaleTo(2, 500, Easing.BounceOut);
+            await myBouncingBox.ScaleTo(1, 500, Easing.BounceOut);
+            myBouncingBox.CornerRadius = 10;
+            await myBouncingBox.TranslateTo(-100, 100, 500, Easing.Linear);
+            await myBouncingBox.TranslateTo(100, 100, 500, Easing.Linear);
+            await myBouncingBox.TranslateTo(0, 0, 500, Easing.Linear);
+            await myBouncingBox.FadeTo(0, 500, Easing.BounceIn);
+            await myBouncingBox.FadeTo(1, 500, Easing.BounceIn);
+
+        }
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Day7/WebViewSample/WebViewSample/MainPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace WebViewSample
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public void Handle_Clicked(object sender, EventArgs e)
+        {
+            if(!tbxDireccion.Text.StartsWith("https://", StringComparison.CurrentCulture))
+            {
+                tbxDireccion.Text = "https://"+tbxDireccion.Text;
+            }
+            myWebBrowser.Source = tbxDireccion.Text;
+        }
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
