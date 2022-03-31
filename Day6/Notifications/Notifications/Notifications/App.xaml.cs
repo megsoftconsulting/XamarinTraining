f2f3e68eeef7958d3db1ad47fa0453c22202bb01
Carlos Campos <me@carloscampos.dev>
2022-03-31 17:29:39 -0600

    Setup new project for local notification samples

---
+++ Day6/Notifications/Notifications/Notifications/App.xaml.cs
@@ -0,0 +1,33 @@
+﻿using Notifications.Views;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+[assembly: XamlCompilation(XamlCompilationOptions.Compile)]
+
+namespace Notifications
+{
+    public partial class App
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new NavigationPage(new MainPage());
+        }
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
